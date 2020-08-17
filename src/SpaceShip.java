cbf66c7933fcada27154692e83438bd81d19c65d
Kashif_Ghafoor <58185339+kashif-ghafoor@users.noreply.github.com>
2020-08-16 18:42:56 -0700

    Update SpaceShip.java

--- src/SpaceShip.java
+++ src/SpaceShip.java
@@ -1,8 +1,8 @@
 
 //interface
 public interface SpaceShip<T> {//using generics
-    public boolean launch();
-    public boolean land();
-    public boolean canCarry(T item);
-    public void carry(T item);
+    boolean launch();
+    boolean land();
+    boolean canCarry(T item);
+    void carry(T item);
 }
